4cc8907cf0b9fcbbb1e66d6515586ce404aaa74e
Prasanna Anbazhagan <praslnx8@gmail.com>
2023-12-30 21:23:03 +0530

    added projected value

--- lib/domain/models/goal.dart
+++ lib/domain/models/goal.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:wealth_wave/api/db/app_database.dart';
 import 'package:wealth_wave/contract/goal_importance.dart';
 import 'package:wealth_wave/domain/models/investment.dart';
@@ -23,6 +25,40 @@ class Goal {
       required this.targetDate,
       required this.importance,
       required this.taggedInvestments});
+
+  double getIrr() {
+    double totalValue = 0.0;
+    double weightedSum = 0.0;
+
+    for (var investment in taggedInvestments.keys) {
+      double share = taggedInvestments[investment] ?? 0;
+      double investedAmount = investment.value * share;
+      double growthRate = investment.getIrr() ?? 0;
+
+      totalValue += investedAmount;
+      weightedSum += investedAmount * growthRate;
+    }
+
+    if (totalValue == 0.0) {
+      return 0.0;
+    }
+
+    return weightedSum / totalValue;
+  }
+
+  double getInvestedValue() {
+    return taggedInvestments.keys
+        .map((e) => e.value * (taggedInvestments[e] ?? 0))
+        .reduce((a, b) => a + b);
+  }
+
+  double getProjectedValue() {
+    double years = DateTime.now().difference(targetDate).inDays / 365;
+    double growthRate = getIrr();
+    double investedValue = getInvestedValue();
+
+    return investedValue * pow(1 + growthRate, years);
+  }
 
   static Goal from(
           {required final GoalDO goal,
