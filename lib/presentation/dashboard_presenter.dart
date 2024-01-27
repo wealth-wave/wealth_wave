d197d6d646de754552bc53efd077f12f23aa3958
Prasanna Anbazhagan <praslnx8@gmail.com>
2024-01-27 07:02:00 +0530

    Add Dashboard Page

--- lib/presentation/dashboard_presenter.dart
+++ lib/presentation/dashboard_presenter.dart
@@ -9,7 +9,41 @@ class DashboardPresenter extends Presenter<DashboardViewState> {
       : _investmentService = investmentService ?? InvestmentService(),
         super(DashboardViewState());
 
-  void fetchDashboard() {}
+  void fetchDashboard() {
+    _investmentService.get().then((investments) {
+      double invested = 0;
+      double currentValue = 0;
+      Map<RiskLevel, double> riskComposition = {};
+      Map<String, double> basketComposition = {};
+      Map<double, double> irrComposition = {};
+
+      for (var investment in investments) {
+        double investmentValue = investment.getValueOn(date: DateTime.now());
+        invested += investment.getTotalInvestedAmount(till: DateTime.now());
+        currentValue += investmentValue;
+        riskComposition.update(
+            investment.riskLevel, (value) => value + investmentValue,
+            ifAbsent: () => investmentValue);
+        basketComposition.update(
+            investment.basketName ?? '-', (value) => value + investmentValue,
+            ifAbsent: () => investmentValue);
+        irrComposition.update((investment.getIRR()).roundToDouble(),
+            (value) => value + investmentValue,
+            ifAbsent: () => investmentValue);
+      }
+
+      updateViewState((viewState) {
+        viewState.invested = invested;
+        viewState.currentValue = currentValue;
+        viewState.riskComposition = riskComposition
+            .map((key, value) => MapEntry(key, value / currentValue));
+        viewState.basketComposition = basketComposition
+            .map((key, value) => MapEntry(key, value / currentValue));
+        viewState.irrComposition = irrComposition
+            .map((key, value) => MapEntry(key, value / currentValue));
+      });
+    });
+  }
 }
 
 class DashboardViewState {
