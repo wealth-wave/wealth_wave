9163d5eaeb73559c3e2716eaad33b3df1be80e7d
Prasanna Anbazhagan <praslnx8@gmail.com>
2024-04-27 13:11:06 +0530

    Add script execution service

--- test/domain/services/dsl_parser_test.dart
+++ test/domain/services/dsl_parser_test.dart
@@ -26,4 +26,42 @@ compute: multiplyBy(5)''';
     expect(actual.computeOperation, equals(expected.computeOperation));
     expect(actual.computeOn, equals(expected.computeOn));
   });
+
+  test('should parse script without query params', () {
+    const String script = '''apiUrl: http://api.stock.com/funds/:fundId/details
+pathParams: fundId=123
+queryParams: 
+responseJsonPath: \$.data.details.value
+compute: multiplyBy(5)''';
+    final ParsedScript actual = dslParser.parse(script);
+    final ParsedScript expected = ParsedScript(
+        url: 'http://api.stock.com/funds/123/details',
+        responsePath: '\$.data.details.value',
+        computeOperation: 'multiplyBy',
+        computeOn: 5);
+
+    expect(actual.url, equals(expected.url));
+    expect(actual.responsePath, equals(expected.responsePath));
+    expect(actual.computeOperation, equals(expected.computeOperation));
+    expect(actual.computeOn, equals(expected.computeOn));
+  });
+
+  test('should parse script without path params', () {
+    const String script = '''apiUrl: http://api.stock.com/funds/1/details
+pathParams: 
+queryParams: 
+responseJsonPath: \$.data.details.value
+compute: multiplyBy(5)''';
+    final ParsedScript actual = dslParser.parse(script);
+    final ParsedScript expected = ParsedScript(
+        url: 'http://api.stock.com/funds/1/details',
+        responsePath: '\$.data.details.value',
+        computeOperation: 'multiplyBy',
+        computeOn: 5);
+
+    expect(actual.url, equals(expected.url));
+    expect(actual.responsePath, equals(expected.responsePath));
+    expect(actual.computeOperation, equals(expected.computeOperation));
+    expect(actual.computeOn, equals(expected.computeOn));
+  });
 }
